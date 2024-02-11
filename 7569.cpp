33d7c7e4774909a98bd01211fe664010c7ebc89e
yeonseong-Lee <yeonseong.dev@gmail.com>
2024-02-12 01:12:53 +0900

    7569.cpp

--- 7569.cpp
+++ 7569.cpp
@@ -1,14 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define X first
-#define Y second
 
 int M, N, H;
-int arr[100][100][100];
 int dx[6] = {0, 0, 1, -1, 0, 0};
 int dy[6] = {1, -1, 0, 0, 0, 0};
 int dz[6] = {0, 0, 0, 0, 1, -1};
+
+int board[103][103][103];
+int dist[103][103][103];
+queue<tuple<int,int,int>> Q;
+
 
 int main(void) {
   ios::sync_with_stdio(0);
@@ -17,26 +19,52 @@ int main(void) {
   cin >> M >> N >> H;
 
   for (int i = 0; i < H; i++) {
-    for (int j = 0; j < M; j++) {
-      for (int k = 0; k < N; k++) {
-        arr[i][j][k] = 1;
+    for (int j = 0; j < N; j++) {
+      for (int k = 0; k < M; k++) {
+        cin >> board[i][j][k];
+        if (board[i][j][k] == 1) {
+          Q.push({i, j, k});
+        }
+        if (board[i][j][k] == 0) {
+          dist[i][j][k] = -1;
+        }
       }
     }
   }
 
-  for (int i = 0; i < 10; i++) {
-    for (int j = 0; j < 10; j++) {
-      for (int k = 0; k < 10; k++) {
-        cout << arr[i][j][k] << " ";
+  while (!Q.empty()) {
+    auto cur = Q.front();
+    Q.pop();
+    int curX, curY, curZ;
+    tie(curZ, curY, curX) = cur;
+
+    for (int dir = 0; dir < 6; dir++) {
+      int nx = curX + dx[dir];
+      int ny = curY + dy[dir];
+      int nz = curZ + dz[dir];
+      if (nx < 0 || ny < 0 || nz < 0 || nx >= M || ny >= N || nz >= H) {
+        continue;
       }
-      cout << "\n";
+      if (dist[nz][ny][nx] >= 0) {
+        continue;
+      }
+      dist[nz][ny][nx] = dist[curZ][curY][curX] + 1;
+      Q.push({nz,ny,nx});
     }
   }
-  
-  
 
-  cout << "WTF";
-  
+  int ans = 0;
+  for (int i = 0; i < H; i++) {
+    for (int j = 0; j < N; j++) {
+      for (int k = 0; k < M; k++) {
+        if (dist[i][j][k] == -1) {
+          cout << -1 << "\n";
+          return 0;
+        }
+        ans = max(ans, dist[i][j][k]);
+      }
+    }
+  }
+  cout << ans << "\n";
+  return 0;
 }
-
-
