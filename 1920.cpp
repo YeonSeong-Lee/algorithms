18c32ea88a1897b64069591e5c2fa14b442b5bbe
yeonseong-Lee <yeonseong.dev@gmail.com>
2024-03-24 23:29:31 +0900

    1920

--- 1920.cpp
+++ 1920.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <algorithm>
-#include <unordered_map>
+#include <set>
 using namespace std;
 
 int main() {
@@ -8,12 +8,41 @@ int main() {
 	cin.tie(0);
   int N, M;
   int temp;
-	unordered_map<int, bool> A;
+	unordered_set<int> A;
+  cin >> N;
+  for (int i = 0; i <#include <iostream>
+#include <algorithm>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+  int N, M;
+  int temp;
+	unordered_set<int> A;
   cin >> N;
   for (int i = 0; i < N; i++) {
 		int temp;
     cin >> temp;
-		A.insert({temp, true});
+		A.insert(temp);
+  }
+  cin >> M;
+  for (int i = 0; i < M; i++) {
+    cin >> temp;
+    if (A.find(temp) != A.end()) {
+      cout << 1 << "\n";
+    }
+    else {
+      cout << 0 << "\n";
+    }
+  }
+  
+  return 0;
+} N; i++) {
+		int temp;
+    cin >> temp;
+		A.insert(temp);
   }
   cin >> M;
   for (int i = 0; i < M; i++) {
