c248ad895056be9a09973bdb63782d8524b43248
yeonseong-Lee <yeonseong.dev@gmail.com>
2024-03-24 23:30:42 +0900

    1920

--- 1920.cpp
+++ 1920.cpp
@@ -1,17 +1,5 @@
 #include <iostream>
 #include <algorithm>
-#include <set>
-using namespace std;
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-  int N, M;
-  int temp;
-	unordered_set<int> A;
-  cin >> N;
-  for (int i = 0; i <#include <iostream>
-#include <algorithm>
 #include <unordered_set>
 using namespace std;
 
@@ -23,23 +11,6 @@ int main() {
 	unordered_set<int> A;
   cin >> N;
   for (int i = 0; i < N; i++) {
-		int temp;
-    cin >> temp;
-		A.insert(temp);
-  }
-  cin >> M;
-  for (int i = 0; i < M; i++) {
-    cin >> temp;
-    if (A.find(temp) != A.end()) {
-      cout << 1 << "\n";
-    }
-    else {
-      cout << 0 << "\n";
-    }
-  }
-  
-  return 0;
-} N; i++) {
 		int temp;
     cin >> temp;
 		A.insert(temp);
