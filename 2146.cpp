007f287518a1bb2b55945519ebba5ae06c70889c
YeonSeong-Lee <yeonseong.dev@gmail.com>
2024-03-26 21:17:55 +0900

    BOJ: 2146
    USE QUEUE!!!

--- 2146.cpp
+++ 2146.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -7,17 +7,17 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
     int N;
-    vector<int> v;
+    queue<int> q;
     cin >> N;
     for (int i = 0; i < N; i++) {
-        v.push_back(i + 1);
+        q.push(i + 1);
     }
-    while (v.size() > 1)
+    while (q.size() > 1)
     {
-        v.erase(v.begin());
-        v.push_back(*v.begin());
-        v.erase(v.begin());
+        q.pop();
+        q.push(q.front());
+        q.pop();
     }
-    cout << v.front();
+    cout << q.front();
     return 0;
 }
