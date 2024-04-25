576838cc355db56bc4462ebd94ead8dbacac1555
yeonseong <yeonseong.dev@gmail.com>
2024-04-25 08:56:23 +0000

    wip

--- 1665.cpp
+++ 1665.cpp
@@ -18,5 +18,32 @@ int main()
         int temp;
         cin >> temp;
         v.push_back(temp);
+        if (temp > max)
+        {
+            max = temp;
+        }
     }
+    int start = 1;
+    int end = max;
+    int mid = (start + end) / 2;
+    while (start < mid)
+    {
+        cout << "start " << start << " " << "mid " << mid << " end " << end << "\n";
+        int temp = 0;
+        for (int i = 0; i < v.size(); i++)
+        {
+            temp += v[i] / mid;
+            cout << "temp :" << temp << " v[i] / mid " << v[i] / mid << "\n";
+        }
+        if (temp > N)
+        {
+            start = mid;
+        }
+        else
+        {
+            end = mid;
+        }
+        mid = (start + end) / 2;
+    }
+    cout << mid;
 }
