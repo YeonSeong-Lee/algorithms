353070b266ecbfbcb097fab1c83284a9fd9d1a2e
yeonseong <yeonseong.dev@gmail.com>
2024-05-19 15:24:28 +0900

    Add solution for problem 1463

---
+++ class3/1463.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+if (n == 1):
+  print(1)
+  exit()
+if (n == 2):
+  print(1)
+  exit()
+if (n == 3):
+  print(1)
+  exit()
+
+dp = [int(1e9)] * (n + 1)
+dp[1] = 0
+dp[2] = 1
+dp[3] = 1
+
+
+for i in range(4, n + 1):
+  dp[i] = min(dp[i], dp[i - 1] + 1)
+  if (i % 3 == 0):
+    dp[i] = min(dp[i], dp[i // 3] + 1)
+  if (i % 2 == 0):
+    dp[i] = min(dp[i], dp[i // 2] + 1)
+
+
+print(dp[n])
