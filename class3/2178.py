7b689cdc3a5c9088f3f98ca8b71fd0f0fd8c071b
yeonseong <yeonseong.dev@gmail.com>
2024-05-20 15:29:48 +0000

    BFS, 최단거리

--- class3/2178.py
+++ class3/2178.py
@@ -4,34 +4,30 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 info = []
-visited = [[False] * 100 for _ in range(100)]
+visited = [[0] * 101 for _ in range(101)]
 
 for _ in range(n):
     v = input()
-    info.append(v)
+    info.append(v)  
 
 dir = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 q = deque()
 q.append((1, 1))
-visited[1][1] = True
-cnt = 1
+visited[1][1] = 1
 while q:
     y, x = q.popleft()
+    if x == m and y == n:
+        break
     for dy, dx in dir:
         ny, nx = y + dy, x + dx
         if (nx < 1 or nx > m or ny < 1 or ny > n):
             continue
-        if (info[ny - 1][nx - 1] == 0):
+        if (info[ny - 1][nx - 1] == '0'):
             continue
-        if (visited[ny][nx] == True):
-            continue
-        if (nx == m - 1 or ny == n - 1):
+        if (visited[ny][nx] >= 1):
             continue
         q.append((ny, nx))
-        visited[ny][nx] = True
-        cnt += 1
+        visited[ny][nx] = visited[y][x] + 1
 
-print(cnt)
-        
-
+print(visited[n][m])
 
