d925b58b07614122f09671911d7c5a62b3d6b63d
yeonseong <yeonseong.dev@gmail.com>
2024-06-02 12:46:06 +0000

    에

--- class3/2579.py
+++ class3/2579.py
@@ -2,14 +2,16 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-score = [0] * 301
+dp = [0] * 301
 staris = [0] * 301
 
 for i in range(1, n+1):
     staris[i] = int(input())
-score[1] = staris[1]
-score[2] = staris[2]
-# TODO: 다시풀기, 맨처음 조건들이 어디까지 갈지 생각해보기. 이 경우는 3까지 초기값임.
+dp[1] = staris[1]
+dp[2] = staris[2] + staris[1]
+dp[3] = max(staris[1] + staris[3], staris[2] + staris[3])
 
-
-print(score[n])
+for i in range(4, n+1):
+    dp[i] += staris[i]
+    dp[i] += max(dp[i-3] + staris[i-1], dp[i-2])
+print(dp[n])
