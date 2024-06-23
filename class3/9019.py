e731cd4d98dfd2402cb7c4372e82c0b8ddaf99f4
YeonSeong-Lee <yeonseong.dev@gmail.com>
2024-06-23 20:34:47 +0900

    python3

--- class3/9019.py
+++ class3/9019.py
@@ -1,28 +1,39 @@
 import sys
-input = sys.stdin.readline
 from collections import deque
+input = sys.stdin.readline
 
-n = int(input())
-dir = ['D', 'S', 'L', 'R']
-# 유의!: 어디를 방문처리할지 생각!
-def bfs(oper, fr, to):
-    if fr == to:
-        print(oper)
-    for v in dir:
-        oper = oper + v
-        if v == 'D':
-            temp = D(fr)
-            bfs(oper, temp, to)
-        elif v == 'S':
-            temp = S(fr)
-            bfs(oper, temp, to)
-        elif v == 'L':
-            temp = L(fr)
-            bfs(oper, temp, to)
-        elif v == 'R':
-            temp = R(fr)
-            bfs(oper, temp, to)
+t = int(input())
 
-for _ in range(n):
-    fr, to = map(int, input().split())
-    bfs(fr, to)
+
+def min_command(a, b):
+    visited = [0 for _ in range(10001)]
+    dir = ['D', 'S', 'L', 'R']
+    q = deque()
+    q.append((a, ''))
+    visited[a] = 1
+    while q:
+        cur_num, cur_command = q.popleft()
+        if cur_num == b:
+            print(cur_command)
+            break
+        for v in dir:
+            temp = 0
+            if v == 'D':
+                temp = (2 * cur_num) % 10000
+            if v == 'S':
+                temp = cur_num - 1
+                if cur_num == 0:
+                    temp = 9999
+            if v == 'L':
+                temp = (cur_num % 1000) * 10 + cur_num // 1000
+            if v == 'R':
+                temp = (cur_num % 10) * 1000 + (cur_num - cur_num % 10) // 10
+            if visited[temp] == 1:
+                continue
+            visited[temp] = 1
+            q.append((temp, cur_command + v))
+
+
+for _ in range(t):
+    a, b = map(int, input().split())
+    min_command(a, b)
