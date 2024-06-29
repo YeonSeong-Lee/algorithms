437befa3fa4534987b615a1366c0d05a531a5dfb
yeonseong-Lee <yeonseong.dev@gmail.com>
2024-06-30 03:29:42 +0900

    dp, 가장 긴 부분 수열

---
+++ boj/marathon/11053.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().strip())
+nums = list(map(int, input().split()))
+dp = [1 for _ in range(n+1)]
+dp[1] = 1
+
+for i in range(1, n+1):
+  for j in range(1, i):
+    if nums[i-1] > nums[j-1]:
+      dp[i] = max(dp[i], dp[j] + 1)
+
+print(max(dp))
