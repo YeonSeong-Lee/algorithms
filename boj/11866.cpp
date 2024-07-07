830d43393a7e54fa6e5da9d7a42d9c21e10ca46f
yeonseong <yeonseong.dev@gmail.com>
2024-07-07 22:43:04 +0900

    wip

--- boj/11866.cpp
+++ boj/11866.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 int main() {
     int N, K;
-    int loop = 0;
     vector<int> v;
 
     cin >> N >> K;
@@ -13,10 +12,14 @@ int main() {
     for (int i = 0; i < N; i++){
         v.push_back(i + 1);
     }
+    cout << "<";
+    int idx = K - 1;
     while (!v.empty()) {
-        loop++;
-        int idx = (loop * K) % v.size();
-        cout << "idx: " << idx << '\n';
+        cout << v[idx];
         v.erase(v.begin() + idx);
+        if (v.empty()) break;
+        idx = (idx + K - 1) % v.size();
+        cout << ", ";
     }
+    cout << ">";
 }
