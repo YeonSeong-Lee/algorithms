319d930c36ca3789a91aeaefae7af6c6f78627d4
yeonseong <yeonseong.dev@gmail.com>
2024-07-07 22:52:55 +0900

    다각형 오목, 볼록 여부 잘 살파기

--- boj/gold/2166.py
+++ boj/gold/2166.py
@@ -5,20 +5,20 @@ def get_area_in_triangle(a, b, c):
   x1, y1 = a
   x2, y2 = b
   x3, y3 = c
-  return abs(x1*y2 + x2*y3 + x3*y1 - x2*y1 - x3*y2 - x1*y3) / 2
+  return (x1*y2 + x2*y3 + x3*y1 - x2*y1 - x3*y2 - x1*y3)
 
 n = int(input())
-veterx = []
+vertices = []
 total = 0
 
 for _ in range(n):
   x, y = map(int, input().split())
-  veterx.append((x,y))
+  vertices.append((x,y))
 
-a = veterx[0]
+a = vertices[0]
 for i in range(1, n-1):
-  b = veterx[i]
-  c = veterx[i+1]
+  b = vertices[i]
+  c = vertices[i+1]
   total += get_area_in_triangle(a, b, c)
 
-print(round(total, 1))
+print(round(abs(total / 2), 1))
