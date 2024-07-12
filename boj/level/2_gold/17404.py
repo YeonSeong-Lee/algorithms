045d5f7a0ae79551529fb9ec1f0a123ac10b0f03
yeonseong <yeonseong.dev@gmail.com>
2024-07-12 15:12:53 +0000

    fix

--- boj/level/2_gold/17404.py
+++ boj/level/2_gold/17404.py
@@ -12,9 +12,9 @@ for i in range(3):
   dp = [[1e9] * 3 for _ in range(n)]
   dp[0][i] = colors[0][i]
   for j in range(1, n):
-    dp[j][0] = min(dp[j-1][1], dp[j-1][2]) + colors[j-1][0]
-    dp[j][1] = min(dp[j-1][0], dp[j-1][2]) + colors[j-1][1]
-    dp[j][2] = min(dp[j-1][0], dp[j-1][1]) + colors[j-1][2]
+    dp[j][0] = min(dp[j-1][1], dp[j-1][2]) + colors[j][0]
+    dp[j][1] = min(dp[j-1][0], dp[j-1][2]) + colors[j][1]
+    dp[j][2] = min(dp[j-1][0], dp[j-1][1]) + colors[j][2]
   dp[-1][i] = 1e9
   ans = min(ans, min(dp[-1]))
 
