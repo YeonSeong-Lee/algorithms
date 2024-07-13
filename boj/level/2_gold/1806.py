10bd5d7a71f1523e358ae55cbefb54efaf213873
yeonseong <yeonseong.dev@gmail.com>
2024-07-13 19:07:18 +0900

    two pointer

--- boj/level/2_gold/1806.py
+++ boj/level/2_gold/1806.py
@@ -1,20 +1,22 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
 n, s = map(int, input().split())
 nums = list(map(int, input().split()))
-ans = []
-r_ans = []
 
-def dfs(start):
-    if sum(ans) >= s:
-        r_ans.append(len(ans))
-        return
-    for i in range(start, len(nums)):
-        ans.append(nums[i])
-        dfs(i+1)
-        ans.pop()
+start, end = 0, 0
+current_sum = 0
+min_length = n + 1
 
-dfs(0)
-print(min(r_ans) if len(r_ans) > 0 else 0)
+while True:
+    if current_sum >= s:
+        min_length = min(min_length, end - start)
+        current_sum -= nums[start]
+        start += 1
+    elif end == n:
+        break
+    else:
+        current_sum += nums[end]
+        end += 1
+
+print(min_length if min_length != n + 1 else 0)
