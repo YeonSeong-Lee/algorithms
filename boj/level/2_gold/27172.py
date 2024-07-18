f51dc2fb18ad49a611dcec06df4ce3f0e04e319b
yeonseong-Lee <yeonseong.dev@gmail.com>
2024-07-19 04:31:36 +0900

    Refactor code for finding the smallest number with k distinct digits

--- boj/level/2_gold/27172.py
+++ boj/level/2_gold/27172.py
@@ -5,16 +5,18 @@ input = sys.stdin.readline
 n = int(input())
 nums = list(map(int, input().split()))
 score = [0] * n
+max_num = max(nums)
+nums_index = {num: idx for idx, num in enumerate(nums)}
 
 for i in range(n):
-    for j in range(i, n):
-        player = nums[i]
-        opponent = nums[j]
-        if opponent % player == 0:
+    cur = nums[i]
+    for j in range(cur * 2, max_num + 1, cur):
+        if j in nums_index:
             score[i] += 1
-            score[j] -= 1
-        elif player % opponent == 0:
-            score[i] -= 1
-            score[j] += 1
+            idx = nums_index[j]
+            score[idx] -= 1
 
 print(*score)
+
+# 배운 점
+# 정렬이 필요한데 원래 순서를 기억해야할 필요가 있을 때는 dict로 인덱스를 기억하고 있기!
