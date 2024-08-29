1626e9b418cef9275f584ced72f60e73546df944
yeonseong <yeonseong.dev@gmail.com>
2024-08-29 13:24:52 +0000

    feat: Add code for finding the nearest tallest building using a stack

---
+++ boj/level/2_gold/25556.py
@@ -0,0 +1,21 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n = int(input())
+nums = list(map(int, input().split()))
+
+stacks = [deque() for _ in range(4)]
+
+for e in nums:
+    flag = False
+    for s in stacks:
+        if not s or (s and s[-1] < e):
+            s.append(e)
+            flag = True
+            break
+    if flag == False:
+        print("NO")
+        exit()
+
+print("YES")
