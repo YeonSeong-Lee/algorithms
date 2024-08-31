b5d3dce4ee3d9dbb2223c48488f70e2547a52ae9
YeonSeong-Lee <yeonseong.dev@gmail.com>
2024-08-31 22:06:16 +0900

    --wip-- [skip ci]

--- boj/level/2_gold/32203.py
+++ boj/level/2_gold/32203.py
@@ -7,12 +7,18 @@ n, m = map(int, input().split())
 graph = defaultdict(list)
 c = list(map(int, input().split()))
 
-males = []
-females = []
+for i, e in enumerate(c):
+    if e % 2 == 0:
+        c[i] = 0
+    else:
+        c[i] = 1
+
 root = [0] * (n + 1)
 rank = [0] * (n + 1)
+candidates = [0] * (n)
 for i in range(1, n + 1):
     root[i] = i
+    candidates[i-1] = i
 
 def find(x):
     if x == root[x]:
@@ -28,16 +34,9 @@ def union(x, y):
     if rank[x] < rank[y]:
         x, y = y, x
     root[y] = x
+    candidates.remove(y)
     if rank[x] == rank[y]:
         rank[x] += 1
-
-def find_all_set():
-    candidates = []
-    for e in range(1, n + 1):
-        temp = root[e]
-        if temp not in candidates:
-            candidates.append(temp)
-    return candidates
 
 def find_couple(root):
     elements = []
@@ -47,26 +46,16 @@ def find_couple(root):
             elements.append(i)
     comb = list(combinations(elements, 2))
     
-    for e in comb:
-        if e[0] in males and e[1] in females:
-            cnt += 1
-        if e[0] in females and e[1] in males:
+    for a, b in comb:
+        if c[a-1] != c[b-1]:
             cnt += 1
     return cnt
-    
-    
 
-for i, e in enumerate(c):
-    if e % 2 == 0:
-        females.append(i + 1)
-    else:
-        males.append(i + 1)
 
 
 for _ in range(m):
     a, b = map(int, input().split())
     union(a, b)
-    candidates = find_all_set()
     ans = 0
     for candidate in candidates:
         ans += find_couple(candidate)
